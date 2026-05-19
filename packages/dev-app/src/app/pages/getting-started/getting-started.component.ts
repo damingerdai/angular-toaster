27b24f9a831ccf177327059f660cbf92f435cc0b
damingerdai <mingguobin@live.com>
2026-05-19 13:59:17 +0800

    chore: disable rerender

--- packages/dev-app/src/app/pages/getting-started/getting-started.component.ts
+++ packages/dev-app/src/app/pages/getting-started/getting-started.component.ts
@@ -3,8 +3,20 @@ import { afterNextRender, Component, inject } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { Marked } from 'marked';
 import { markedHighlight } from "marked-highlight";
-import hljs from 'highlight.js';
-import { defer, map, race, switchMap } from 'rxjs';
+import { defer, map, race, Subscription, switchMap } from 'rxjs';
+
+import hljs from 'highlight.js/lib/core';
+import typescript from 'highlight.js/lib/languages/typescript';
+import javascript from 'highlight.js/lib/languages/javascript';
+import xml from 'highlight.js/lib/languages/xml';
+import css from 'highlight.js/lib/languages/css';
+import bash from 'highlight.js/lib/languages/bash';
+
+hljs.registerLanguage('typescript', typescript);
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('xml', xml);
+hljs.registerLanguage('css', css);
+hljs.registerLanguage('bash', bash);
 
 @Component({
   selector: 'app-getting-started',
@@ -18,11 +30,23 @@ export class GettingStartedComponent {
   private readonly chinaDocs = 'https://gh-proxy.com/raw.githubusercontent.com/damingerdai/angular-toaster/refs/heads/develop/packages/angular-toaster/README.md';
   protected loading = true;
   protected markdownContent: SafeHtml | null = null;
-  protected marked: Marked;;
-  httpClient = inject(HttpClient);
-  sanitizer = inject(DomSanitizer);
+  protected marked!: Marked;;
+  private httpClient = inject(HttpClient);
+  private sanitizer = inject(DomSanitizer);
+  private sub = new Subscription();
 
   constructor() {
+    this.initMarked();
+
+    afterNextRender(() => {
+      this.fetchData();
+    })
+
+
+    // You can inject HttpClient here if needed
+  }
+
+  private initMarked() {
     this.marked = new Marked(
       markedHighlight({
         emptyLangClass: 'hljs',
@@ -33,25 +57,19 @@ export class GettingStartedComponent {
         }
       })
     );
-
-    afterNextRender(() => {
-      this.fetchData();
-    })
-
-
-    // You can inject HttpClient here if needed
   }
 
   fetchData() {
     const reqs = [
       this.httpClient.get(this.docs, { responseType: 'text' }),
-      this.httpClient.get(this.chinaDocs, { responseType: 'text' })];
-    race(...reqs).pipe(
+      this.httpClient.get(this.chinaDocs, { responseType: 'text' })
+    ];
+
+    this.sub = race(...reqs).pipe(
       switchMap((data) => defer(() => this.marked.parse(data, { async: true }))),
       map(html => this.sanitizer.bypassSecurityTrustHtml(html))
     ).subscribe({
       next: (data) => {
-        console.log(data);
         this.markdownContent = data;
         this.loading = false;
       },
@@ -60,5 +78,9 @@ export class GettingStartedComponent {
         this.loading = false;
       }
     });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 }
